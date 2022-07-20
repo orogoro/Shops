ed38f9b3e0fa883d0f971a94bffb9812a96347f3
Yurii Dvornichenko <raen98@live.ru>
2022-07-20 13:12:32 +0300

    fix build

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import store, { persistor } from './redux/store';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import store, { persistor } from './redux/store';
 import { App } from 'components/App';
-import { Loader } from 'components/shops';
+// import { Loader } from 'components/shops';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      {/* <PersistGate loading={<Loader />} persistor={persistor}> */}
-      <BrowserRouter basename="/Shops/">
-        <App />
-      </BrowserRouter>
-      {/* </PersistGate> */}
-    </Provider>
+    {/* <Provider store={store}> */}
+    {/* <PersistGate loading={<Loader />} persistor={persistor}> */}
+    <BrowserRouter basename="/Shops/">
+      <App />
+    </BrowserRouter>
+    {/* </PersistGate> */}
+    {/* </Provider> */}
   </React.StrictMode>
 );
