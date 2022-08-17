bfa898708fd55fe1ec914875dedbcab9320ba515
orogoro <64734994+orogoro@users.noreply.github.com>
2022-08-17 11:42:29 +0000

    Deploying to gh-pages from @ orogoro/Shops@8a0e89eebbbdc5997c09b6cbe81f6023c21ce588 🚀

---
+++ static/js/881.1173cb78.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[881],{8881:function(e,t,r){r.r(t);var a=r(3071),n=r(3329);t.default=function(){return(0,n.jsx)("main",{children:(0,n.jsx)(a.YZ,{})})}}}]);
+//# sourceMappingURL=881.1173cb78.chunk.js.map
